3b004885a26b48c30cfba60642381063d80b38c3
Foragger <robertostick1997@gmail.com>
2024-08-21 11:56:58 -0300

    Haciendo mi primer commit sobre la app

---
+++ src/FirstApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirstApp = () => {
+  return (
+    <h2>FirstApp</h2>
+  )
+}
+
+export default FirstApp;
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+// function App() {
+//     // document.createElement...
+//     return (<h1>Hola mundo!</h1>);
+// }
+
+
+// export default App;
+
+//import React from 'react'
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+}
+
+
+// export const HelloWorldApp = () => <h1>Arrow Function</h1>;
